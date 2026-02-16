01967957364410298355f5cc7f0aaf3099adba0f
NiiAmateyTagoe <armahtey@gmail.com>
2026-02-15 21:03:46 -0600

    first commit

---
+++ src/main/java/org/harmony/cinema_room_rest_service/CinemaRoomRestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.harmony.cinema_room_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRoomRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRoomRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/harmony/cinema_room_rest_service/CinemaRoomRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.harmony.cinema_room_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRoomRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
